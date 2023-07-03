1f8dcb297287cb5ea493f58136daadf72862873c
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-07-03 22:55:42 +0300

    feat: write snapshot tests and correct table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -3,55 +3,46 @@ import { simpleCalculator, Action } from './index';
 
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  //{ a: 2, b: 2, action: Action.Add, expected: 4 },
+  //{ a: 3, b: 2, action: Action.Add, expected: 5 },
 
   { a: 1, b: 2, action: Action.Subtract, expected: -1 },
-  { a: 2, b: 2, action: Action.Subtract, expected: 0 },
-  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+  //{ a: 2, b: 2, action: Action.Subtract, expected: 0 },
+  //{ a: 3, b: 2, action: Action.Subtract, expected: 1 },
 
   { a: 1, b: 2, action: Action.Multiply, expected: 2 },
-  { a: 2, b: 2, action: Action.Multiply, expected: 4 },
-  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  //{ a: 2, b: 2, action: Action.Multiply, expected: 4 },
+  //{ a: 3, b: 2, action: Action.Multiply, expected: 6 },
 
   { a: 1, b: 2, action: Action.Divide, expected: 0.5 },
-  { a: 2, b: 2, action: Action.Divide, expected: 1 },
-  { a: 3, b: 2, action: Action.Divide, expected: 1.5 },
+  //{ a: 2, b: 2, action: Action.Divide, expected: 1 },
+  //{ a: 3, b: 2, action: Action.Divide, expected: 1.5 },
 
   { a: 1, b: 2, action: Action.Exponentiate, expected: 1 },
-  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
-  { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+  //{ a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
+  //{ a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
 
   { a: 1, b: 2, action: 'Some Action', expected: null },
-  { a: 2, b: 2, action: undefined, expected: null },
-  { a: 3, b: 2, action: NaN, expected: null },
+  //{ a: 2, b: 2, action: undefined, expected: null },
+  //{ a: 3, b: 2, action: NaN, expected: null },
 
   { a: '1', b: '2', action: Action.Add, expected: null },
-  { a: NaN, b: '2', action: Action.Divide, expected: null },
-  { a: undefined, b: '2', action: Action.Multiply, expected: null },
+  //{ a: NaN, b: '2', action: Action.Divide, expected: null },
+  //{ a: undefined, b: '2', action: Action.Multiply, expected: null },
   // continue cases for other actions
 ];
 
 describe('simpleCalculator', () => {
   // Consider to use Jest table tests API to test all cases above
 
-  test.each(testCases.filter((el) => typeof el.expected === 'number'))(
-    'do math %o',
-    (testCase) => {
-      const { a, b, action, expected } = testCase;
-      const result = simpleCalculator({ a, b, action });
-      if (typeof expected === 'number') {
-        expect(result).toBeCloseTo(expected);
-      }
-    },
-  );
-
-  test.each(testCases.filter((el) => el.expected === null))(
-    'invalid action or arguments %o',
-    (testCase) => {
-      const { a, b, action } = testCase;
-      const result = simpleCalculator({ a, b, action });
+  test.each(testCases)('do math %o', (testCase) => {
+    const { a, b, action, expected } = testCase;
+    const result = simpleCalculator({ a, b, action });
+    if (typeof expected === 'number') {
+      expect(result).toBeCloseTo(expected);
+    }
+    if (expected === null) {
       expect(result).toBeNull();
-    },
-  );
+    }
+  });
 });
--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,36 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
     // Write your test here
+
+    const elements = [1, 2];
+    const expected = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: null,
+          next: null,
+        },
+      },
+    };
+
+    const actual = generateLinkedList(elements);
+
+    expect(actual).toStrictEqual(expected);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
     // Write your test here
+
+    const elements = [3, 2, 1];
+
+    const actual = generateLinkedList(elements);
+
+    expect(actual).toMatchSnapshot();
   });
 });
